2a1d8bbc6ee71d303e8f39f4a51f4a1c19475d1f
VHriho <v12grig12@gmail.com>
2023-12-10 22:50:38 +0200

    added solution of Problem2

--- CtrlWork1/Main.hs
+++ CtrlWork1/Main.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 {- PROBLEM 1
 It is often needed to convert a number written in arabic symbols,
 to a string of its textual representation, i.e. for financial documents.
@@ -19,10 +21,33 @@ strNum x = listOfNum !! x
 
 listOfNum = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
+{- PROBLEM 2
+Write a function findMaxFrequency that for a given homogenous list of type a
+returns a pair (a, Int) of the most frequent element (any, if there are more than one) and its frequecy. For an empty list throw an error. 
+-}
+
+findMaxFrequency :: (Ord a) => [a] -> (Int, a)
+findMaxFrequency []  = error "Empty list"
+findMaxFrequency lst = maximum (freq lst)
+
+freq x = map(sorted)(list x)
+
+sorted x = (length(head(list x)), head(head(list x)))
+
+list x = group(sort x)
+
 problem1 = do
     print (intToWords  150)  -- "one-five-zero"
     print (intToWords    0)  -- "zero"
     print (intToWords (-10)) -- "minus-one-zero"
 
+problem2 = do
+  print "Problem 2"
+  print $ findMaxFrequency [1,2,1,3,1,4]   -- (1, 3)
+  print $ findMaxFrequency [1,1,2,2]       -- (1, 2) or (2, 2)
+  print $ findMaxFrequency "some sentence" -- ('e', 4)
+  print $ findMaxFrequency ([] :: String)  -- error
+
 main = do
   problem1
+  problem2
